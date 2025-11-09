c40d851625fee9cd9db3ef77be620b1d5a9f1b5a
ppruvost <patrick.pruvost50@gmail.com>
2025-11-09 15:57:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: 'https://<ton-utilisateur>.github.io'  // remplace par ton nom GitHub
+    origin: 'https://ppruvost.github.io'  // remplace par ton nom GitHub
 }));
 
 app.post('/send-email', async (req, res) => {
